14a50a2845c7611837ee4e92f1c348de0f3d7438
Tehora Dutenu <tehoradutenu@gmail.com>
2025-07-01 16:10:59 +0200

    styles & favicon

---
+++ wp-content/themes/lhostal/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link rel="icon" href="<?php echo get_template_directory_uri() . 'favicon.ico'; ?>" type="image/x-icon">
+	<!-- Google Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+	<!-- Bootstrap Icons -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">        <!-- Styles -->
+    <!-- Mes styles -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . "/style.css"; ?>">
+
+	<title>L'Hostal</title>
+</head>
+<body>
+	
+</body>
+</html>
