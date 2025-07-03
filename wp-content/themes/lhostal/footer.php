5a85c473e2c0659373756a17dd11f1e2fcbb92b7
Tehora Dutenu <tehoradutenu@gmail.com>
2025-07-03 17:59:52 +0200

    Footer ok - Bandeau_img first

--- wp-content/themes/lhostal/footer.php
+++ wp-content/themes/lhostal/footer.php
@@ -1,11 +1,13 @@
 <footer>
 	<div class="footer">
-		<div class="adresse">
-			<h5>ADRESSE</h5>
-			<p>13 carrer Na Patora<br>66300 Castelnou</p>
+		<div class="poste">
+			<h5><b>ADRESSE</b></h5>
+			<img class="footer_separateur" src="<?php echo get_template_directory_uri() . '/img/separateur.png'; ?>" alt="séparateur">
+			<p class="adresse"><b>13 carrer Na Patora<br> 66300 CASTELNOU</b></p>
 		</div>
 		<div class="reseau">
-			<h5>SUIVEZ-NOUS !</h5>
+			<h5><b>SUIVEZ-NOUS !</b></h5>
+			<img class="footer_separateur" src="<?php echo get_template_directory_uri() . '/img/separateur.png'; ?>" alt="séparateur">
 			<div class="icones">
 				<a href="https://www.facebook.com/lhostal.castelnou/?locale=fr_FR">
 					<img class="picto" src="<?php echo get_template_directory_uri() . '/img/facebook_blc.png'; ?>" alt="Facebook de l'Hostal">
@@ -16,11 +18,13 @@
 			</div>
 		</div>
 		<div class="phone">
-			<h5>TÉLÉPHONE</h5>
-			<p class="num">04 68 53 45 42</p>
+			<h5><b>TÉLÉPHONE</b></h5>
+			<img class="footer_separateur" src="<?php echo get_template_directory_uri() . '/img/separateur.png'; ?>" alt="séparateur">
+			<a class="num" href="tel:+33468534542">04 68 53 45 42</a>
 		</div>
 	</div>
 </footer>
 
 </body>
 </html>
+
--- wp-content/themes/lhostal/front-page.php
+++ wp-content/themes/lhostal/front-page.php
@@ -6,6 +6,31 @@
 
 get_header(); ?>
 
-<main></main>
+<main>
+	<div class="first_bloc">
+		<div class="bloc_img">
+			<div class="band_horaires"> <!-- terrasse_enseigne-->
+				<div class="overlay_1"></div>
+				<h6><b>CUISINE CATALANE ~ BAR</b></h6>
+				<h5><b>OUVERT TOUS LES JOURS DE 9h30 À 21h30</b></h5>
+				<p>Cuisine ouverte de 12h à 21h30 non-stop sur réservation</p>
+			</div>
+			<div class="band_terrasse"> <!-- table_pierre -->
+				<div class="overlay_1"></div>
+				<h5><b>TERRASSE PANORAMIQUE</b></h5>
+				<h5><b>LARGE CHEMINÉE INTÉRIEURE</b></h5>
+			</div>
+			<div class="band_grill">
+				<div class="overlay_1"></div>
+				<h6><b>GRILLADES ~ CARGOLADE*</b></h6>
+				<p><b>Tous les jours du 1er juillet au 31 août</b></p>
+				<p class="asterisque"><b>* cargolade sur réservation</b></p>
+			</div>
+		</div>
+	</div>
+	<div class="presentation">
+
+	</div>
+</main>
 
 <?php get_footer(); ?>
